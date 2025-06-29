0800085ff6e906a3fdf5d575c4f0c9a423a34c21
Supavit D <supavit.dchan@gmail.com>
2025-06-29 18:32:41 +1000

    Update

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 rsync -av .config/ $HOME/.config/
 
-if ! grep -q 'starship init bash' ~/.bashrc; then
-    echo 'eval "$(starship init bash)"' >> ~/.bashrc
+echo 'export LANG=en_US.UTF-8' >> ~/.zshrc
+echo 'export LC_ALL=en_US.UTF-8' >> ~/.zshrc
+
+if ! grep -q 'starship init bash' ~/.zshrc; then
+    echo 'eval "$(starship init bash)"' >> ~/.zshrc
+fi
+
+if ! grep -q '[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh' ~/.zshrc; then
+    echo '[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh"' >> ~/.zshrc
 fi
